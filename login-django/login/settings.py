ec4cd92d049816c404628610648f03afc50eac21
Akbar-deve <akbarpasha8088@gmail.com>
2023-07-21 11:17:34 +0530

    3rd commit

--- login-django/login/settings.py
+++ login-django/login/settings.py
@@ -74,15 +74,31 @@ WSGI_APPLICATION = 'login.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#          'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'Logindata',
+#         'USER': 'postgres',
+#         'PASSWORD':'root',
+#         'HOST': 'localhost',
+#     }
+# }
+
+
+
+
 DATABASES = {
     'default': {
-         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Logindata',
-        'USER': 'postgres',
-        'PASSWORD':'root',
-        'HOST': 'localhost',
+         'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        
     }
 }
+
+
+
+
+
 AUTHENTICATION_BACKENDS = [
      'django.contrib.auth.backends.ModelBackend',  # Default Django authentication 
 ]
